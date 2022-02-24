aef3d0c4594013b32003539569e941eb45dc8d1b
borisov.alex12@gmail.com <borisov.alex12@gmail.com>
2022-02-24 20:39:23 +0300

    quiz fragment

--- app/src/main/java/com/example/soccerquiz/MainActivity.kt
+++ app/src/main/java/com/example/soccerquiz/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.soccerquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.soccerquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
