3833980f59a78ddfc77c19be0b8ea50a69c7733f
borisov.alex12@gmail.com <borisov.alex12@gmail.com>
2022-02-26 10:32:16 +0300

    quiz fragment

--- app/src/main/java/com/example/soccerquiz/QuizFragment.kt
+++ app/src/main/java/com/example/soccerquiz/QuizFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.soccerquiz.databinding.FragmentQuizBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +35,8 @@ class QuizFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false)
+        val binding: FragmentQuizBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_quiz, container, false)
+        return binding.root
     }
 
     companion object {
